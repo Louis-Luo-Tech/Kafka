4b3968ffcaed29a605fe9f4088d35879b81f5a22
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-04-18 15:57:45 -0700

    kafka Spark Streaming example

--- build.sbt
+++ build.sbt
@@ -2,4 +2,10 @@ name := "Kafka"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.10"
+
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.4"
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.5.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.4"
--- src/main/scala/Kafka.scala
+++ src/main/scala/Kafka.scala
@@ -1,5 +1,39 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 object Kafka {
- def main(args: Array[String]): Unit ={
-   print(1)
- }
+  def main(args: Array[String]): Unit = {
+    val brokers = "localhost:9092"
+    val groupid = "GRP1"
+    val topics = "kafkaexample"
+    val sparkconf = new SparkConf().setMaster("local[*]").setAppName("kafkastreaming")
+    val ssc = new StreamingContext(sparkconf,Seconds(3))
+    val sc = ssc.sparkContext
+    sc.setLogLevel("OFF")
+    val topicSet = topics.split(",").toSet
+    val kafkaParams = Map[String,Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
+      ConsumerConfig.GROUP_ID_CONFIG -> groupid,
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
+    )
+    val message = KafkaUtils.createDirectStream[String,String](
+      ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String,String](topicSet,kafkaParams)
+    )
+    val line = message.map(_.value)
+    val words = line.flatMap(_.split(" "))
+    val wordsCount = words.map(x => (x, 1L)).reduceByKey(_+_)
+    wordsCount.print()
+    ssc.start()
+    ssc.awaitTermination()
+//    start zookeeper server
+//    start kafka server
+//    ./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic kafkaexample
+//    run this object
+//    type "hello world" in kafka server
+
+  }
 }
